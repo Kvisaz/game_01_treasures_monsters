2d11f4194835ee00cc9ef843147e8b13b7c2e83c
sergeytokarev <stokarev@novakidschool.com>
2024-08-11 20:31:03 +0700

    TreasuresAndMonsters2 -  draggy 2

--- src/components/TreasuresAndMonsters2/components/HexGrid/index.ts
+++ src/components/TreasuresAndMonsters2/components/HexGrid/index.ts
@@ -1,4 +1,4 @@
-import { forEach2D, GameObject, setLeftTop } from "../../../../common";
+import { addCameraDraggingToObject, forEach2D, GameObject, setLeftTop } from "../../../../common";
 import Pointer = Phaser.Input.Pointer;
 
 type ColumnRow = { column: number, row: number };
@@ -9,12 +9,17 @@ interface IProps {
   cellBuilder: (columnRow: ColumnRow) => GameObject;
 }
 
+const CLICK_EVENT = Phaser.Input.Events.GAMEOBJECT_POINTER_UP;
+const DRAG_TIME_BEFORE_CLICK = 500;
+
 /** to do если не будет привязки к логике - выноси в общие **/
 export class HexGrid extends Phaser.GameObjects.Container {
 
   private readonly unSubs: (() => void)[] = [];
   /** обработчик кликов и драггинга камеры **/
   private readonly inputRect: Phaser.GameObjects.Rectangle;
+  private isInputDragging = false;
+  private resetCamera?: () => void;
 
   constructor(private props: IProps) {
     super(props.scene);
@@ -25,18 +30,20 @@ export class HexGrid extends Phaser.GameObjects.Container {
       const gameObject = cellBuilder(columnRow);
       this.add(gameObject);
     });
-    const clickables = [...this.list];
 
     this.inputRect = this.createInputRect();
     this.addAt(this.inputRect);
 
+    this.bindEvents();
+
     props.scene.add.existing(this);
-    this.unSubs.push(setupCameraDrag(this.scene, this.inputRect, clickables as GameObject[]));
   }
 
   destroy(fromScene?: boolean) {
     super.destroy(fromScene);
     this.unSubs.forEach(unSub => unSub());
+    this.inputRect.destroy();
+    this.resetCamera?.();
   }
 
   /**
@@ -51,88 +58,63 @@ export class HexGrid extends Phaser.GameObjects.Container {
 
     return rect;
   }
-}
 
-function setupCameraDrag(scene: Phaser.Scene, inputRect: GameObject, clickables?: GameObject[]) {
-  let isDragging = false;
-  let isClickLocked = false;
-  const camera = scene.cameras.main;
-  let startX: number, startY: number;
+  private bindEvents() {
+    this.inputRect.setInteractive({
+      useHandCursor: true,
+      draggable: true,
+    });
 
-  const prevX = camera.scrollX;
-  const prevY = camera.scrollY;
-  //
-  // inputRect.setInteractive({
-  //   useHandCursor: true,
-  //   draggable: true
-  // });
+    this.bindClick();
+    this.bindDraggingCamera();
+    this.bindDisableClicksWhenDragging();
+  }
 
-  scene.input.on(Phaser.Input.Events.POINTER_DOWN, (pointer: Pointer) => {
-    console.log("GAMEOBJECT_DRAG_START");
-    isDragging = true;
-    isClickLocked = true;
-    startX = pointer.x + camera.scrollX;
-    startY = pointer.y + camera.scrollY;
-  });
-
-  scene.input.on(Phaser.Input.Events.POINTER_MOVE, (pointer: Pointer) => {
-    if (!isDragging) return;
-    console.log("POINTER_MOVE");
-    const deltaX = startX - pointer.x;
-    const deltaY = startY - pointer.y;
-
-    // Ограничиваем движение камеры размерами контейнера
-    camera.scrollX = Phaser.Math.Clamp(
-      deltaX,
-      0,
-      Math.max(0, inputRect.width - camera.width)
-    );
-    camera.scrollY = Phaser.Math.Clamp(
-      deltaY,
-      0,
-      Math.max(0, inputRect.height - camera.height)
-    );
-  });
-
-  scene.input.on('pointerup', () => {
-    console.log("pointerup");
-    isDragging = false;
-    inputRect.scene.time.addEvent({
-      delay: 500,
-      callback: () => {
-        isClickLocked = false;
-        console.log("GAMEOBJECT_DRAG_END fired");
+  private bindClick() {
+    /** Я выбираю такой способ привязки эвентов
+     * чтобы не перегружать код обычных тайлов
+     * и иметь возможность разруливать настройку драггинга и кликов в одном месте
+     **/
+    this.inputRect.on(CLICK_EVENT, (pointer: Pointer) => {
+      const { worldX: x, worldY: y } = pointer;
+      if (this.isInputDragging) {
+        return;
       }
-    });
-  });
+      console.log('click on cell');
+      // const cell = this.getCellByXY(x, y);
 
-  // scene.input.on('pointerup', () => {
-  //   console.log("GAMEOBJECT_DRAG_END");
-  //   isDragging = false;
-    inputRect.scene.time.addEvent({
-      delay: 500,
-      callback: () => {
-        isClickLocked = false;
-        console.log("GAMEOBJECT_DRAG_END fired");
-      }
+      // if (cell == null) {
+      //   console.warn("critical error: cell undefined!");
+      //   console.warn(`x ${x} y ${y}`);
+      //   return;
+      // } else {
+      //   // this.props.onClick(cell);
+      // }
     });
-  // });
+  }
 
-  // Добавляем обработку кликов по ячейкам
-  clickables?.forEach((cell: GameObject) => {
-    cell.setInteractive();
-    cell.on('pointerup', (pointer: Pointer, localX: number, localY: number, e: { stopPropagation: () => void }) => {
-      if (isClickLocked) return;
-        // Обработка клика по ячейке
-        console.log("Клик по ячейке:", cell);
-        // Здесь можно добавить вашу логику обработки клика
-        e.stopPropagation(); // Предотвращаем распространение события
+
+
+
+  private bindDraggingCamera() {
+    const rect = this.inputRect;
+    const { width, height } = rect.getBounds();
+
+    this.resetCamera = addCameraDraggingToObject(rect, width, height);
+  }
+
+  private bindDisableClicksWhenDragging() {
+    this.inputRect.on(Phaser.Input.Events.GAMEOBJECT_DRAG_START, () => {
+      this.isInputDragging = true;
     });
-  });
 
-  return () => {
-    console.log('unsub');
-    camera.scrollX = prevX;
-    camera.scrollY = prevY;
-  };
+    this.inputRect.on(Phaser.Input.Events.GAMEOBJECT_DRAG_END, () => {
+      this.inputRect.scene.time.addEvent({
+        delay: DRAG_TIME_BEFORE_CLICK,
+        callback: () => {
+          this.isInputDragging = false;
+        },
+      });
+    });
+  }
 }
