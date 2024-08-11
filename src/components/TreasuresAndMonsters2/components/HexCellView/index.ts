e4120939662267ed527da651f0261cf6110572a2
sergeytokarev <stokarev@novakidschool.com>
2024-08-11 19:33:07 +0700

    TreasuresAndMonsters2 - hexcell view stroke

--- src/components/TreasuresAndMonsters2/components/HexCellView/index.ts
+++ src/components/TreasuresAndMonsters2/components/HexCellView/index.ts
@@ -3,14 +3,15 @@ import { cssColorToInt } from "../../../../common";
 interface IProps {
   scene: Phaser.Scene;
   color: string;
+  strokeColor?: string;
   cellSize: number;
 }
 
 /** to do если не будет привязки к логике - выноси в общие **/
 export class HexCellView extends Phaser.GameObjects.Ellipse {
-  constructor({ scene, color, cellSize }: IProps) {
+  constructor({ scene, color, cellSize, strokeColor = "#2a2f2a" }: IProps) {
     super(scene, 0, 0, cellSize, cellSize, cssColorToInt(color));
-    this.setStrokeStyle(2, cssColorToInt("#2a2f2a"));
+    this.setStrokeStyle(2, cssColorToInt(strokeColor));
     this.setSmoothness(6);
   }
 }
