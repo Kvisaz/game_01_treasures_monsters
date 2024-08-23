7d52909e539a383d3d65343308029a20719678b5
sergeytokarev <stokarev@novakidschool.com>
2024-08-23 20:10:31 +0700

    TreasuresAndMonsters2 -  asyncFlow draft

--- src/components/gamestates/TMGameState/TMGameState.story.ts
+++ src/components/gamestates/TMGameState/TMGameState.story.ts
@@ -12,8 +12,17 @@ export const tmGameStateStory: IStory = {
     const { cancel, run } = asyncFlow([
       async () => console.log("state changes started"),
       () => delay(1000),
+      async () => {
+        console.log("changes 1");
+      },
       () => delay(1000),
+      async () => {
+        console.log("changes 2");
+      },
       () => delay(1000),
+      async () => {
+        console.log("changes 3");
+      },
       async () => console.log("state changes finished")
     ]);
     run().catch(console.warn);
