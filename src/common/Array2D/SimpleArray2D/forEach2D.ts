27056404d63b9f07478e86a22aa2e5f5644a1c2e
sergeytokarev <stokarev@novakidschool.com>
2024-08-25 19:45:37 +0700

    forEach2D logging

--- src/common/Array2D/SimpleArray2D/forEach2D.ts
+++ src/common/Array2D/SimpleArray2D/forEach2D.ts
@@ -3,9 +3,17 @@ export function forEach2D<T>(
   callback: (value: T, col: number, row: number) => boolean | void
 ) {
   const columns = arr.length;
-  if (columns == 0) return;
+  if (columns <= 0) {
+    console.warn('columns <= 0');
+    return;
+  }
+
   const rows = arr[0].length;
-  if (rows == 0) return;
+  if (rows <= 0) {
+    console.warn('rows <= 0');
+    return;
+  }
+
   for (let col = 0; col < columns; col++) {
     for (let row = 0; row < rows; row++) {
       if (callback(arr[col][row], col, row)) return;
